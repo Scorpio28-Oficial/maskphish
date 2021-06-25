4768c69a6922b65ecf7ded976865829f5aa2a599
Scorpio28-Oficial <77907586+Scorpio28-Oficial@users.noreply.github.com>
2021-06-24 22:31:29 -0500

    Update maskphish.sh

--- maskphish.sh
+++ maskphish.sh
@@ -9,33 +9,67 @@ url_checker() {
     fi
 }
 
-echo -e "\n\e[1;31;42m######┌──────────────────────────┐##### \e[0m"
-echo -e "\e[1;31;42m######│▙▗▌      ▌  ▛▀▖▌  ▗    ▌  │##### \e[0m"
-echo -e "\e[1;31;42m######│▌▘▌▝▀▖▞▀▘▌▗▘▙▄▘▛▀▖▄ ▞▀▘▛▀▖│##### \e[0m"
-echo -e "\e[1;31;42m######│▌ ▌▞▀▌▝▀▖▛▚ ▌  ▌ ▌▐ ▝▀▖▌ ▌│##### \e[0m"
-echo -e "\e[1;31;42m######│▘ ▘▝▀▘▀▀ ▘ ▘▘  ▘ ▘▀▘▀▀ ▘ ▘│##### \e[0m"
-echo -e "\e[1;31;42m######└──────────────────────────┘##### \e[0m \n"
+echo -e "\n\e[1;30;42m######┌──────────────────────────┐##### \e[0m"
+echo -e "\e[1;30;42m######│▙▗▌      ▌  ▛▀▖▌  ▗    ▌  │##### \e[0m"
+echo -e "\e[1;30;42m######│▌▘▌▝▀▖▞▀▘▌▗▘▙▄▘▛▀▖▄ ▞▀▘▛▀▖│##### \e[0m"
+echo -e "\e[1;30;42m######│▌ ▌▞▀▌▝▀▖▛▚ ▌  ▌ ▌▐ ▝▀▖▌ ▌│##### \e[0m"
+echo -e "\e[1;30;42m######│▘ ▘▝▀▘▀▀ ▘ ▘▘  ▘ ▘▀▘▀▀ ▘ ▘│##### \e[0m"
+echo -e "\e[1;30;42m######└──────────────────────────┘##### \e[0m \n"
 echo -e "\e[40;38;5;82m Por Favor Visite \e[30;48;5;82m https://t.me/Darkmux_Community \e[0m"
 echo -e "\e[30;48;5;82m    Comunidad \e[40;38;5;82m   Traducido Por Scorpio28 \e[0m \n\n"
-echo -e "\e[1;31;42m ### Phishing URL ###\e[0m \n"
-echo -n "Pegue la URL de phishing aquí (con http o https): "
+echo -e "\e[1;30;42m ### Phishing URL ###\e[0m \n"
+echo -e -n "
+\e[1;30m┌═══════════════════════════════┐
+█\e[1;32m Pegue la URL de phishing aquí\e[1;30m █
+█\e[1;30m (\e[1;37mcon http o https\e[1;30m)            █
+└═══════════════════════════════┘
+┃
+└═>>>\e[1;37m "
 read phish
 url_checker $phish
 sleep 1
-echo "Procesamiento y modificación de url de phishing"
+echo -e "
+\e[1;30m┌══════════════════════════════┐
+█\e[1;32m Procesamiento y modificación\e[1;30m █
+█\e[1;32m de url de phishing\e[1;30m           █
+└══════════════════════════════┘"
 echo ""
 short=$(curl -s https://is.gd/create.php\?format\=simple\&url\=${phish})
 shorter=${short#https://}
-echo -e "\n\e[1;31;42m ### Masking Dominio ###\e[0m"
-echo 'Dominio para enmascarar la URL de phishing (con http or https), ex: https://google.com, http
-://anything.org) :'
+echo -e "\n\e[1;30;42m ### Masking Dominio ###\e[0m"
+echo -e -n "
+\e[1;30m┌══════════════════════════════════════════════┐
+█\e[1;32m Dominio para enmascarar la URL de phishing\e[1;30m   █
+█\e[1;30m (\e[1;37mcon http or https\e[1;30m)\e[1;37m, ex: https://google.com,\e[1;30m █
+█\e[1;37m http://anything.org\e[1;30m)                         █
+└══════════════════════════════════════════════┘
+┃
+└═>>>\e[1;37m "
 echo -en "\e[32m=>\e[0m "
 read mask
 url_checker $mask
-echo -e '\nEscriba palabras de ingeniería social:(como el dinero gratis, los mejores trucos de publicación)'
-echo -e "\e[31mNo use el espacio, solo use '-' entre las palabras de ingeniería social\e[0m"
+echo -e '\n
+\e[1;30m┌════════════════════════════════════════════════════════════┐
+█\e[1;32m Escriba palabras de ingeniería social:\e[1;30m                     █
+█\e[1;30m (\e[1;37mcomo el dinero gratis, los mejores trucos de publicación\e[1;30m) █
+└════════════════════════════════════════════════════════════┘'
+echo -e "
+\e[1;31m┌═══════════════════════════════════════┐
+█\e[1;32m No use el espacio, solo use \e[1;30m'\e[1;37m-\e[1;30m'\e[1;32m entre\e[1;31m █
+█\e[1;32m las palabras de ingeniería social\e[1;31m     █
+└═══════════════════════════════════════┘"
 echo -en "\e[32m=>\e[0m "
 read words
-echo -e "\nGenerando el enlace MaskPhish ...\n"
+echo -e "\n
+echo -e "
+\e[1;30m┌═══════════════════════════════════┐
+█\e[1;32m Generando el enlace MaskPhish ...\e[1;30m █
+└═══════════════════════════════════┘\n"
 final=$mask-$words@$shorter
-echo -e "Aquí está la URL de MaskPhish:\e[32m ${final} \e[0m\n"
+echo -e "
+echo -e "
+\e[1;30m┌═══════════════════════════════════┐
+█\e[1;32m Aquí está la URL de MaskPhish\e[1;30m █
+└═══════════════════════════════════┘
+┃
+└═>>> \e[1;37mm ${final} \e[0m\n"
